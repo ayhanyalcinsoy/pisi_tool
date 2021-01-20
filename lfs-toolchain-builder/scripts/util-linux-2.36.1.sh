07cf20a6904f33aa4557879a350372839ad07b76
Ayhan Yalçınsoy <ayhanyalcinsoy@pisilinux.org>
2021-01-20 16:45:22 +0300

    update

--- lfs-toolchain-builder/scripts/util-linux-2.36.1.sh
+++ lfs-toolchain-builder/scripts/util-linux-2.36.1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# 5.33. Util-linux-2.32.1
+# 5.33. Util-linux-2.36.1
 set -e
 cd $LFS/sources
-rm -rf util-linux-2.32.1
-tar xf util-linux-2.32.1.tar.xz
-cd util-linux-2.32.1
+rm -rf util-linux-2.36.1
+tar xf util-linux-2.36.1.tar.xz
+cd util-linux-2.36.1
 
 ./configure --prefix=/tools                \
             --without-python               \
@@ -16,4 +16,4 @@ make
 make install
 
 cd $LFS/sources
-rm -rf util-linux-2.32.1
+rm -rf util-linux-2.36.1
